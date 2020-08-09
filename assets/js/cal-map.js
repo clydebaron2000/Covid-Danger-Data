b37707600e598a31d950e96fa4f88c2972800120
Plover Brown <rellwoos@gmail.com>
2020-08-08 20:24:20 -0700

    rate per 100,000 working on click

--- assets/js/cal-map.js
+++ assets/js/cal-map.js
@@ -9,23 +9,6 @@ let newData;
 
 //AJAX QUERIES
 
-//query current and past county data
-
-// let csvCounty = "San Diego"
-// let csvURL = "https://data.ca.gov/api/3/action/datastore_search?resource_id=926fd08f-cc91-4828-af38-bd45de97f8c3&q=" + csvCounty; + "&page=2"
-
-let csvSource = "https://data.ca.gov/dataset/590188d5-8545-4c93-a9a0-e230f0db7290/resource/926fd08f-cc91-4828-af38-bd45de97f8c3/download/statewide_cases.csv";
-let csvCounty = "&county=San Diego"
-let csvURL = "https://c19d.zzzkitty.com/?source=" + csvSource + csvCounty;
-
-$.ajax({
-    url: csvURL,
-    method: "GET"
-}).then(function(response){
-    console.log(response);
-    countyData = response;
-    const anotherThing = findMostRecent(countyData, moment());
-})
 
 
 //query US census 2019 data
@@ -66,8 +49,37 @@ function displayCounty(e) {
     //locate info
     const countyName = $(this).attr("id").replace(/_/g, " ").replace("1", "");
     console.log(countyName);
-    const countyData = findCounty(countyName);
+    countyData = findCounty(countyName);
     console.log(countyData);
+
+        
+
+
+
+
+        //query current and past county data
+
+    // let csvCounty = "San Diego"
+    // let csvURL = "https://data.ca.gov/api/3/action/datastore_search?resource_id=926fd08f-cc91-4828-af38-bd45de97f8c3&q=" + csvCounty;
+
+    let csvSource = "https://data.ca.gov/dataset/590188d5-8545-4c93-a9a0-e230f0db7290/resource/926fd08f-cc91-4828-af38-bd45de97f8c3/download/statewide_cases.csv";
+    let csvCounty = "&county=" + countyName;
+    let csvURL = "https://c19d.zzzkitty.com/?source=" + csvSource + csvCounty;
+
+    $.ajax({
+        url: csvURL,
+        method: "GET"
+    }).then(function(response){
+        console.log(response);
+        const twoWeekTotal = setLast14(response);
+        console.log(`twoWeekTotal = ${twoWeekTotal}`);
+        const ratePer100000 = calculateRate(twoWeekTotal, californiaPopulationData["San Diego"]);
+        console.log(`Rate per 100,000 = ${ratePer100000}`);
+    })
+
+
+
+
     
     //display info
     const info = $("#info");
@@ -204,7 +216,16 @@ function findCounty(needle) {
 
   function setLast14(dataset){
     let total = 0;
-    let i = 0;
-
-    dataset[dataset.length - 1].newcountconfirmed;
+    let index = dataset.length - 1;
+    for (let i = 0; i<14; i++) {
+        console.log(`total = ${total} + ${parseInt(dataset[index].newcountconfirmed)}`);
+        if (dataset[index].newcountconfirmed){
+            total += parseInt(dataset[index].newcountconfirmed);
+        }
+        
+        console.log(`New total = ${total}`);
+        index--;
+        console.log(i);
+    }
+    return total;
   }
