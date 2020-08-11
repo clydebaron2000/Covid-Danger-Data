a99f6ce5ee77fe19620091ef3d63606ec9c8b08e
Clyde Baron Rapinan <clydebaron2000@gmail.com>
2020-08-11 16:00:27 -0700

    INIT

---
+++ script.js
@@ -0,0 +1,14 @@
+// $(function() {
+//     // console.log($("#polyLineContainer"));
+//     $(document).hover(function(e) {
+//         console.log($(e.target));
+//         console.log("hi");
+//     });
+// });
+$(document).ready(function() {
+    $(document).hover(function(event) {
+        console.log("hi");
+        console.log(event);
+    }, function() {});
+});
+//
